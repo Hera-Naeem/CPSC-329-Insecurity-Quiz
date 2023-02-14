293153a9d21e11054e029f2d6e561956b93c18c8
John-Ming-Ngo <john.ming.ngo@gmail.com>
2023-02-13 18:58:26 -0700

    Basic structure made to work.

---
+++ src/main/java/org/insecurity_quiz/Quiz_Data_Management/Question.java
@@ -0,0 +1,7 @@
+package org.insecurity_quiz.Quiz_Data_Management;
+
+public abstract class Question {
+    private String display_question;
+
+    public abstract String question_type();
+}
---
+++ src/main/java/org/insecurity_quiz/Quiz_Data_Management/Question_Types/Multiple_Choice_Question.java
@@ -0,0 +1,10 @@
+package org.insecurity_quiz.Quiz_Data_Management.Question_Types;
+
+import org.insecurity_quiz.Quiz_Data_Management.Question;
+
+public class Multiple_Choice_Question extends Question {
+
+    public String question_type() {
+        return "";
+    }
+}
