7dc95a0df88e305df9611d3076b32fbf4d2a352f
fai28 <shafin.fairooz@gmail.com>
2023-03-17 03:37:02 -0600

    Connected fxml file to main

--- src/main/java/org/insecurity_quiz/Main.java
+++ src/main/java/org/insecurity_quiz/Main.java
@@ -1,7 +1,24 @@
 package org.insecurity_quiz;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application{
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("intro.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        launch(args);
     }
 }
