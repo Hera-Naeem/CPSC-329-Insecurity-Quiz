0e26d2425f703fe43bc6aef10ec6e4f7a20748bd
fai28 <shafin.fairooz@gmail.com>
2023-03-30 13:30:53 -0600

    Made updates to fxml files by adding action event to buttons and completing the methods for event handling.

--- src/main/java/org/insecurity_quiz/ShortAnswerController.java
+++ src/main/java/org/insecurity_quiz/ShortAnswerController.java
@@ -2,8 +2,12 @@ package org.insecurity_quiz;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class ShortAnswerController {
     @FXML
@@ -44,6 +48,25 @@ public class ShortAnswerController {
     @FXML
     public void getHintEvent(ActionEvent actionEvent) {
         // TODO: display a hint for the current question
+        // Create a new window
+        Stage hintStage = new Stage();
+        hintStage.setTitle("Hint");
+
+        // Create a label to display the hint
+        Label hintLabel = new Label("Here's a hint for the current question!");
+
+        // Create a button to close the window
+        Button closeButton = new Button("Close");
+        closeButton.setOnAction(e -> hintStage.close());
+
+        // Create a layout for the window
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(hintLabel, closeButton);
+        layout.setAlignment(Pos.CENTER);
+
+        // Set the layout and show the window
+        hintStage.setScene(new Scene(layout));
+        hintStage.show();
     }
 
 }
